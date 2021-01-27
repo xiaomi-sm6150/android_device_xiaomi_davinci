9ca4b635df430dd71bc91417e96a39adaac23274
Arian <arian.kulmer@web.de>
2021-01-27 18:53:29 +0100

    davinci: keyhandler: Pulse the screen on fod key
    Change-Id: Ie761685c67ba50c9744879d10fb9bfb203ebe608

--- keyhandler/src/org/lineageos/keyhandler/KeyHandler.java
+++ keyhandler/src/org/lineageos/keyhandler/KeyHandler.java
@@ -7,27 +7,36 @@
 package org.lineageos.keyhandler;
 
 import android.content.Context;
+import android.content.Intent;
+import android.os.UserHandle;
 import android.util.Log;
 import android.view.KeyEvent;
 
 import com.android.internal.os.DeviceKeyHandler;
 
 public class KeyHandler implements DeviceKeyHandler {
+    private static final boolean DEBUG = true;
     private static final String TAG = "KeyHandler";
+    private static final String DOZE_INTENT = "com.android.systemui.doze.pulse";
     private static final int KEYCODE_FOD = 338;
 
     public KeyHandler(Context context) {
-        Log.i(TAG, "KeyHandler constructor called");
+        mContext = context;
+        if (DEBUG)
+            Log.i(TAG, "KeyHandler constructor called");
     }
 
     public KeyEvent handleKeyEvent(KeyEvent event) {
         int scanCode = event.getScanCode();
-        Log.i(TAG, "handleKeyEvent=" + scanCode);
-        switch (scanCode) {
-            case KEYCODE_FOD:
-                return null;
-            default:
-                return event;
-        }
+        if (scanCode != KEYCODE_FOD)
+            return event;
+
+        if (DEBUG)
+            Log.d(TAG, "Got FOD BTN_INFO event");
+
+        mContext.sendBroadcastAsUser(
+                new Intent(DOZE_INTENT), new UserHandle(UserHandle.USER_CURRENT));
+
+        return null;
     }
 }
