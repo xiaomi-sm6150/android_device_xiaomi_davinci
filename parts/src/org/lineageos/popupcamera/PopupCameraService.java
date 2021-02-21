1e8ca4c77a43254008421c080c419bd731fd28f8
Arian <arian.kulmer@web.de>
2021-02-21 14:57:54 +0100

    davinci: parts: Avoid saving and restoring our own LED effect
    Change-Id: Iaefcc9a89208ff2c5c7a33b99f4cc49426e9179e

--- parts/src/org/lineageos/popupcamera/PopupCameraService.java
+++ parts/src/org/lineageos/popupcamera/PopupCameraService.java
@@ -65,6 +65,10 @@ public class PopupCameraService extends Service implements Handler.Callback {
     private Sensor mFreeFallSensor;
     private SoundPool mSoundPool;
 
+    private boolean mLedBusy = false;
+    private String mLedBreathing = "0";
+    private String mLedBrightness = "0";
+
     private CameraManager.AvailabilityCallback availabilityCallback =
             new CameraManager.AvailabilityCallback() {
                 @Override
@@ -283,9 +287,12 @@ public class PopupCameraService extends Service implements Handler.Callback {
 
     private void lightUp(boolean open) {
         if (mPopupCameraPreferences.isLedAllowed()) {
-            // Save the active LED effects
-            String ledBreathing = FileUtils.readOneLine(Constants.LEFT_LED_BREATH_PATH);
-            String ledBrightness = FileUtils.readOneLine(Constants.LEFT_LED_BRIGHTNESS_PATH);
+            // Save the active LED effects if it's not our effect and mark the LED as busy
+            if (!mLedBusy) {
+                mLedBreathing = FileUtils.readOneLine(Constants.LEFT_LED_BREATH_PATH);
+                mLedBrightness = FileUtils.readOneLine(Constants.LEFT_LED_BRIGHTNESS_PATH);
+            }
+            mLedBusy = true;
 
             // Reset the active LED effects
             setLed("breath", "0");
@@ -310,11 +317,14 @@ public class PopupCameraService extends Service implements Handler.Callback {
                 setLed("step_ms", "70");
 
                 // Restore the previous LED effects
-                if (ledBreathing.equals("1")) {
-                    FileUtils.writeLine(Constants.LEFT_LED_BREATH_PATH, ledBreathing);
+                if (mLedBreathing.equals("1")) {
+                    FileUtils.writeLine(Constants.LEFT_LED_BREATH_PATH, mLedBreathing);
                 } else {
-                    FileUtils.writeLine(Constants.LEFT_LED_BRIGHTNESS_PATH, ledBrightness);
+                    FileUtils.writeLine(Constants.LEFT_LED_BRIGHTNESS_PATH, mLedBrightness);
                 }
+
+                // Unmark the LED as busy since our effect is done
+                mLedBusy = false;
             }, 1400);
         }
     }
