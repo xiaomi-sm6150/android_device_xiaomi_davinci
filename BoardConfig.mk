7f40b4b4288ed4356516ba2ef45ec53589af3ad0
Arian <arian.kulmer@web.de>
2021-10-07 13:31:56 +0200

    davinci: Add build broken rules
    Change-Id: I5f2328e2607088ca21530228141ca37a6ba4e6df

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -4,6 +4,9 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
+BUILD_BROKEN_MISSING_REQUIRED_MODULES := true
+
 # Inherit from sm6150-common
 include device/xiaomi/sm6150-common/BoardConfigCommon.mk
 
